b76cc026e8d00ff5cd988285303c67a0b2f542e1
moamen2020 <m01144944981@gmail.com>
2020-08-27 04:56:04 +0200

    Create Feth Repository

---
+++ main.js
@@ -0,0 +1,37 @@
+// Main Variables
+
+let theInput = document.querySelector(".get-repos input"),
+  getButton = document.querySelector(".get-button"),
+  reposData = document.querySelector(".show-data");
+
+getButton.onclick = function () {
+  getRepos();
+};
+
+// Get Repos Function
+
+function getRepos() {
+  // If Value Is Empty
+  if (theInput.value == "") {
+    reposData.innerHTML = `<span>Please Write Github Username.</span>`;
+  } else {
+    fetch(`https://api.github.com/users/${theInput.value}/repos`)
+      .then((res) => res.json())
+      .then((data) => {
+        // Empty The Container
+        reposData.innerHTML = "";
+
+        // Loop On Repositories
+        data.forEach((repos) => {
+          // Create The Main Div Element
+          let mainDiv = `
+          <div class="repo-box">
+          ${repos.name}
+          <a target=''_blank href="https://github.com/${theInput.value}/${repos.name}">Visit<a/>
+          <span>Stars ${repos.stargazers_count}</span>
+          </div>
+          `;
+        });
+      });
+  }
+}
